e243b6016a43eb58dfa826ee05ebb9c7150cfbda
Oleg Sidorenko <sidorenko6503@gmail.com>
2025-03-30 17:06:34 +0300

    added kafka service for test

---
+++ src/kafka/kafka.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Consumer, Kafka } from 'kafkajs';
+
+@Injectable()
+export class KafkaService implements OnModuleInit {
+  private kafka: Kafka;
+
+  private consumer: Consumer;
+
+
+  constructor() {
+    this.kafka = new Kafka({
+      clientId: 'MisaFiles',
+      brokers: [`localhost:9092`]
+    });
+    this.consumer = this.kafka.consumer({groupId: 'misa-files-group'});
+  }
+
+  async onModuleInit() {
+    try {
+      await this.consumer.connect();
+      await this.consumer.subscribe({ topic: 'file-uploaded', fromBeginning: true });
+
+      await this.consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          console.log(`Received message: ${message?.value?.toString()}; topic: ${topic}`);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
